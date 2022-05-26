28739ff77893faf1aa8111faef74c38592e0ca4d
LuisOlive <lp.olivares@yahoo.com>
2022-05-26 03:51:42 -0500

    promedios

--- voltmetro-arduino.ino
+++ voltmetro-arduino.ino
@@ -1,18 +1,35 @@
-int sensorPin = A0,
-    sensorValue = 0;
+#define N 200 // número de muestras antes de cada corte
+
+double temp, sum = 0;
+byte i = 0;
 
 void setup() {
   Serial.begin(9600);  
 }
 
 void loop() {
-  sensorValue = analogRead(sensorPin);
+  // leyendo, ahora entre 2 terminales
+  int A = analogRead(A0);
+  int B = analogRead(A1);
   
-  Serial.print("El voltaje medido es ");
-  Serial.print(elVoltaje(sensorValue));
+  temp = caidaTension(A - B);
+  sum += temp;
+  
+  // imprimirVoltaje(temp);
+  
+  if(++i == N) {
+    Serial.print("El promedio es ");
+    imprimirVoltaje(sum / i);
+    
+    sum = i = 0;
+  }
+}
+
+void imprimirVoltaje(double v) {
+  Serial.print(v, 7);
   Serial.println(" V");
 }
 
-double elVoltaje(int entradaBits) {
+double caidaTension(int entradaBits) {
   return 5.0 * entradaBits / 1024;  
 }
